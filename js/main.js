437ced1bb74d05b8ad787980b49ec20c59dcc168
Bob Sforza <bsforza70@gmail.com>
2015-07-13 21:34:37 -0500

    Beautified&Concatenated CSS, Cleaned up some js, Hardware Acceleration is back?

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
 	});
 
 	function triggerFilter($bool) {
-		var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
+		var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter')/*, $('.cd-gallery')*/]);
 		elementsToTrigger.each(function(){
 			$(this).toggleClass('filter-is-visible', $bool);
 		});
